f0dd606961dd0e3c0dafb4ca35f45f66b41597bf
ValentinaCastiglioni <vale.castiglioni@gmail.com>
2024-05-03 13:02:36 +0200

    initial commit for marXbot

---
+++ examples/marXbot/src/main/java/it/unicam/quasylab/jspear/examples/marXbot/Main.java
@@ -0,0 +1,270 @@
+/*
+ * JSpear: a SimPle Environment for statistical estimation of Adaptation and Reliability.
+ *
+ *              Copyright (C) 2020.
+ *
+ * See the NOTICE file distributed with this work for additional information
+ * regarding copyright ownership.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *             http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package it.unicam.quasylab.jspear.examples.marXbot;
+
+import it.unicam.quasylab.jspear.*;
+import it.unicam.quasylab.jspear.controller.Controller;
+import it.unicam.quasylab.jspear.controller.ControllerRegistry;
+import it.unicam.quasylab.jspear.ds.*;
+import it.unicam.quasylab.jspear.perturbation.*;
+import it.unicam.quasylab.jspear.feedback.*;
+import org.apache.commons.math3.random.RandomGenerator;
+
+import java.io.IOException;
+import java.util.*;
+
+public class Main {
+
+    public final static String[] VARIABLES =
+            new String[]{"p_speed", "s_speed", "p_distance", "accel", "timer_V", "braking_distance", "gap"
+            };
+
+    public final static double ACCELERATION = 0.25;
+    public final static double BRAKE = 0.5;
+    public final static double NEUTRAL = 0.0;
+    public final static int TIMER = 2;
+    public final static double INIT_SPEED = 0.0;
+    public final static double MAX_SPEED = 3.0;
+    public final static double INIT_X = 0.0;
+    public final static double INIT_Y = 0.0;
+    public final static double INIT_THETA = Math.PI/2;
+    public final static double FINAL_X = 7.0;
+    public final static double FINAL_Y = 19.0;
+    public final static double INIT_DISTANCE = Math.sqrt(Math.pow(FINAL_X-INIT_X,2) + Math.pow(FINAL_Y-INIT_Y,2));
+    private static final int H = 350;
+
+    private static final int x = 0;
+    private static final int y = 1;
+    private static final int theta = 2;
+    private static final int p_speed = 3;
+    private static final int s_speed = 4;
+    private static final int p_distance = 5;
+    private static final int accel = 6;
+    private static final int timer_V = 7;
+    private static final int gap = 8;
+
+    private static final int NUMBER_OF_VARIABLES = 9;
+
+
+
+    public static void main(String[] args) throws IOException {
+        try {
+            Controller vehicle = getController();
+            DataState state = getInitialState();
+            ControlledSystem system = new ControlledSystem(vehicle, (rg, ds) -> ds.apply(getEnvironmentUpdates(rg, ds)), state);
+            EvolutionSequence sequence = new EvolutionSequence(new DefaultRandomGenerator(), rg -> system, 100);
+
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void printData(RandomGenerator rg, String label, DataStateExpression f, SystemState s, int steps, int size) {
+        System.out.println(label);
+        double[] data = SystemState.sample(rg, f, s, steps, size);
+        for (int i = 0; i < data.length; i++) {
+            System.out.printf("%d> %f\n", i, data[i]);
+        }
+    }
+
+    private static void printData(RandomGenerator rg, String label, DataStateExpression f, Perturbation p, SystemState s, int steps, int size) {
+        System.out.println(label);
+        double[] data = SystemState.sample(rg, f, p, s, steps, size);
+        for (int i = 0; i < data.length; i++) {
+            System.out.printf("%d> %f\n", i, data[i]);
+        }
+    }
+
+    private static void printLData(RandomGenerator rg, ArrayList<String> label, ArrayList<DataStateExpression> F, SystemState s, int steps, int size) {
+        System.out.println(label);
+        double[][] data = SystemState.sample(rg, F, s, steps, size);
+        for (int i = 0; i < data.length; i++) {
+            System.out.printf("%d>   ", i);
+            for (int j = 0; j < data[i].length -1; j++) {
+                System.out.printf("%f   ", data[i][j]);
+            }
+            System.out.printf("%f\n", data[i][data[i].length -1]);
+        }
+    }
+
+    private static void printLData(RandomGenerator rg, ArrayList<String> label, ArrayList<DataStateExpression> F, Perturbation p, SystemState s, int steps, int size) {
+        System.out.println(label);
+        double[][] data = SystemState.sample(rg, F, p, s, steps, size);
+        for (int i = 0; i < data.length; i++) {
+            System.out.printf("%d>   ", i);
+            for (int j = 0; j < data[i].length -1; j++) {
+                System.out.printf("%f   ", data[i][j]);
+            }
+            System.out.printf("%f\n", data[i][data[i].length -1]);
+        }
+    }
+
+    private static void printLData_min(RandomGenerator rg, ArrayList<String> label, ArrayList<DataStateExpression> F, SystemState s, int steps, int size) {
+        System.out.println(label);
+        double[][] data = SystemState.sample_min(rg, F, new NonePerturbation(), s, steps, size);
+        for (int i = 0; i < data.length; i++) {
+            System.out.printf("%d>   ", i);
+            for (int j = 0; j < data[i].length -1; j++) {
+                System.out.printf("%f   ", data[i][j]);
+            }
+            System.out.printf("%f\n", data[i][data[i].length -1]);
+        }
+    }
+
+    private static void printLData_max(RandomGenerator rg, ArrayList<String> label, ArrayList<DataStateExpression> F, SystemState s, int steps, int size) {
+        System.out.println(label);
+        double[][] data = SystemState.sample_max(rg, F, new NonePerturbation(), s, steps, size);
+        for (int i = 0; i < data.length; i++) {
+            System.out.printf("%d>   ", i);
+            for (int j = 0; j < data[i].length -1; j++) {
+                System.out.printf("%f   ", data[i][j]);
+            }
+            System.out.printf("%f\n", data[i][data[i].length -1]);
+        }
+    }
+
+    // CONTROLLER OF VEHICLE
+
+    public static Controller getController() {
+
+        ControllerRegistry registry = new ControllerRegistry();
+
+        registry.set("Ctrl",
+                Controller.ifThenElse(
+                        DataState.greaterThan(s_speed, 0),
+                        Controller.ifThenElse(
+                                DataState.greaterThan(gap, 0 ),
+                                Controller.doAction(
+                                        (rg, ds) -> List.of(new DataStateUpdate(accel, ACCELERATION),
+                                                new DataStateUpdate(timer_V, TIMER)),
+                                        registry.reference("Accelerate")
+                                ),
+                                Controller.doAction(
+                                        (rg, ds) -> List.of( new DataStateUpdate(accel, - BRAKE),
+                                                new DataStateUpdate(timer_V, TIMER)),
+                                        registry.reference("Decelerate"))
+                        ),
+                        Controller.ifThenElse(
+                                DataState.greaterThan(gap,0),
+                                Controller.doAction(
+                                        (rg, ds) -> List.of(new DataStateUpdate(accel, ACCELERATION),
+                                                new DataStateUpdate(timer_V, TIMER)),
+                                        registry.reference("Accelerate")
+                                ),
+                                Controller.doAction(
+                                        (rg,ds)-> List.of(new DataStateUpdate(accel,NEUTRAL),
+                                                new DataStateUpdate(timer_V, TIMER)),
+                                        registry.reference("Stop")
+                                )
+                        )
+                )
+        );
+
+        registry.set("Accelerate",
+                Controller.ifThenElse(
+                        DataState.greaterThan(timer_V, 0),
+                        Controller.doTick(registry.reference("Accelerate")),
+                        registry.reference("Ctrl")
+                )
+        );
+
+        registry.set("Decelerate",
+                Controller.ifThenElse(
+                        DataState.greaterThan(timer_V, 0),
+                        Controller.doTick(registry.reference("Decelerate")),
+                        registry.reference("Ctrl")
+                )
+        );
+
+        registry.set("Stop",
+                Controller.ifThenElse(
+                        DataState.greaterThan(timer_V, 0),
+                        Controller.doTick(registry.reference("Stop")),
+                        Controller.doAction((rg,ds)-> List.of(new DataStateUpdate(timer_V, TIMER)),
+                                registry.reference("Stop")
+                        )
+                )
+        );
+
+
+
+        return registry.reference("Ctrl");
+
+    }
+
+
+    // ENVIRONMENT EVOLUTION
+
+    public static List<DataStateUpdate> getEnvironmentUpdates(RandomGenerator rg, DataState state) {
+        List<DataStateUpdate> updates = new LinkedList<>();
+        double new_timer_V = state.get(timer_V) - 1;
+        double new_p_speed;
+        if (state.get(accel) == NEUTRAL) {
+            new_p_speed = Math.max(0.0,state.get(p_speed)-ACCELERATION);
+        } else {
+            new_p_speed = Math.min(MAX_SPEED, Math.max(0, state.get(p_speed) + state.get(accel)));
+        }
+        double token = rg.nextDouble();
+        double new_s_speed = new_p_speed;
+        double newX = state.get(x) + Math.cos(state.get(theta))*new_p_speed;
+        double newY = state.get(y) + Math.sin(state.get(theta))*new_p_speed;
+        //if (token < 0.5){
+        //    new_s_speed = new_p_speed + rg.nextDouble()*0.5;
+        //} else {
+        //    new_s_speed = new_p_speed - rg.nextDouble()*0.5;
+        //}
+        double new_p_distance = Math.sqrt(Math.pow(FINAL_X-newX,2) + Math.pow(FINAL_Y-newY,2));
+        updates.add(new DataStateUpdate(x,newX));
+        updates.add(new DataStateUpdate(y,newY));
+        updates.add(new DataStateUpdate(timer_V, new_timer_V));
+        updates.add(new DataStateUpdate(p_speed, new_p_speed));
+        updates.add(new DataStateUpdate(p_distance, new_p_distance));
+        double new_braking_distance = (Math.pow(new_s_speed,2) + (ACCELERATION + BRAKE) * (ACCELERATION * Math.pow(TIMER,2) + 2 * new_s_speed * TIMER)) / (2 * BRAKE);
+        double new_gap = new_p_distance - new_braking_distance;
+        updates.add(new DataStateUpdate(s_speed, new_s_speed));
+        updates.add(new DataStateUpdate(gap, new_gap));
+        return updates;
+    }
+
+
+    // INITIALISATION OF DATA STATE
+
+    public static DataState getInitialState( ) {
+        Map<Integer, Double> values = new HashMap<>();
+
+        values.put(timer_V, (double) 0);
+        values.put(x, INIT_X);
+        values.put(y, INIT_Y);
+        values.put(theta, INIT_THETA);
+        values.put(p_speed, INIT_SPEED);
+        values.put(s_speed, INIT_SPEED);
+        values.put(p_distance, INIT_DISTANCE);
+        values.put(accel, NEUTRAL);
+        double init_braking_distance = (Math.pow(INIT_SPEED,2) + (ACCELERATION + BRAKE) * (ACCELERATION * Math.pow(TIMER,2) + 2 * INIT_SPEED * TIMER))/(2 * BRAKE);
+        double init_gap = INIT_DISTANCE - init_braking_distance;
+        values.put(gap, init_gap);
+
+        return new DataState(NUMBER_OF_VARIABLES, i -> values.getOrDefault(i, Double.NaN));
+    }
+
+}
