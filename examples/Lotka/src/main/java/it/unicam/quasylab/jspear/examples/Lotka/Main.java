50bf201f62f7cf0692cbb995594507053f24d4d8
simonetini <simone.tini@uninsubria.it>
2024-07-19 14:37:27 +0200

    Simulation of distances

--- examples/Lotka/src/main/java/it/unicam/quasylab/jspear/examples/Lotka/Main.java
+++ examples/Lotka/src/main/java/it/unicam/quasylab/jspear/examples/Lotka/Main.java
@@ -45,11 +45,15 @@ import java.util.*;
 public class Main {
 
     /*
-    General Introduction to Lotka reactions.
+    LOTKA REACTIONS
 
+    The well known set of Lotka reactions consist in the following two autocatalitic reactions
      X + Y1  ---c1---> 2Y1
      Y1 + Y2 ---c2---> 2Y2
+    plus the isomerization
      Y2      ---c3---> Z
+    where it is assumed that the amount of X is insignificantly depleted by the first reaction.
+    The behaviour of Y1 and Y2 is oscillatory.
 
     */
 
@@ -63,11 +67,11 @@ public class Main {
     Technically, for each of the 3 reactions, we define 2 arrays.
     Each of these arrays has 4 positions, which are associated to the four species.
     In particular, the 4 positions are for: X, Y1, Y2, Z.
-    Then, the two arrays for each reaction ri, with i=1,..,4, are:
+    Then, the two arrays for each reaction ri, with i=1,2,3 are:
     - ri_input: position j is 1 if the variable corresponding to position j is a reactant of the reaction
     - ro_output: position j is 1 if the variable corresponding to j is a product of the reaction
     The arrays are defined below.
-
+    For each reaction ri we consider also the constant ri_k carrying the value of ci.
     */
 
 
@@ -101,16 +105,14 @@ public class Main {
 
  VARIABLES MODELING THE STATUS OF THE SYSTEM
 
- Below a list of 7 variables, the idea being that the value of these 30 variables gives a data state, namely an
+ Below a list of 4 variables, the idea being that the value of these 4 variables gives a data state, namely an
  instance of class <code>DataState</code> representing the status of all quantities of the system.
- We have variables for active and inactive promoters, mRNA, proteins and reaction rates. Reaction rates can vary
- since promoter parameters, i.e. the rates of promoter activation and deactivation, depend on the amount of proteins.
  Each variable is associated with an index, from 0 to 29.
  */
  public static final int X = 0; // amount of molecules of X
  public static final int Y1 = 1; // amount of molecules of Y1.
  public static final int Y2 = 2; // amount of molecules of Y2.
- public static final int Z = 3; // amounnt of molecules of Z.
+ public static final int Z = 3; // amount of molecules of Z.
 
  private static final int NUMBER_OF_VARIABLES = 4;
 
@@ -128,7 +130,6 @@ public class Main {
 
             */
 
-
             /*
             One of the elements of a system configuration is the "controller", i.e. an instance of <code>Controller</code>.
             In this example we do not need controllers, therefore we use a controller that does nothing, i.e. an instance
@@ -136,7 +137,7 @@ public class Main {
             In other case studies, controllers may be used to control the activity of a system.
             For instance, a scheduling of a therapy may be modelled by a controller.
             */
-   Controller controller = new NilController();
+            Controller controller = new NilController();
 
             /*
             Another element of a system configuration is the "data state", i.e. an instance of <code>DataState</code>,
@@ -144,10 +145,10 @@ public class Main {
             Instances of <code>DataState</code> contains values for variables representing the quantities of the
             system and four values allowing us to model the evolution of time: gran, Tstep, Treal, Tdelta.
             The initial state <code>state</code> is constructed by exploiting the static method
-            <code>getInitialState</code>, which will be defined later and assigns the initial value to all 30
+            <code>getInitialState</code>, which will be defined later and assigns the initial value to all 4
             variables defined above.
              */
-   DataState state = getInitialState(0.04,0.0,0.0,0.0);
+            DataState state = getInitialState(0.05,0.0,0.0,0.0);
 
             /*
             We define the <code>TimedSystem</code> <code>system</code>, which will be the starting configuration from
@@ -164,9 +165,9 @@ public class Main {
             - an expression over data states, which implements interface <code>DataStateExpression</code> and maps a
             data state <code>ds</code> to the time of next reaction.
              */
-   RandomGenerator rand = new DefaultRandomGenerator();
+            RandomGenerator rand = new DefaultRandomGenerator();
 
-   TimedSystem system = new TimedSystem(controller, (rg, ds) -> ds.apply(selectAndApplyReaction(rg, ds)), state, ds->selectReactionTime(rand,ds));
+            TimedSystem system = new TimedSystem(controller, (rg, ds) -> ds.apply(selectAndApplyReaction(rg, ds)), state, ds->selectReactionTime(rand,ds));
 
 
             /*
@@ -185,7 +186,8 @@ public class Main {
             and sample sets by class <code>SampleSet</code>.
             In this context, <code>size</code> is the cardinality of those sample sets.
             */
-   int size = 1;
+
+            int size = 50;
 
             /*
             The evolution sequence <code>sequence></code> created by the following instruction consists in a sequence of
@@ -194,28 +196,30 @@ public class Main {
             Notice that <code>sequence></code> contains initially only this first sample, the subsequent ones will be
             created "on demand".
              */
-   EvolutionSequence sequence = new EvolutionSequence(rand, rg -> system, size);
+            EvolutionSequence sequence = new EvolutionSequence(rand, rg -> system, size);
 
 
             /*
             Each expression in the following list <code>F</code> allows us to read the value of a given variable
             from a data state
              */
-   ArrayList<DataStateExpression> F = new ArrayList<>();
-   F.add(ds->ds.get(X));
-   F.add(ds->ds.get(Y1));
-   F.add(ds->ds.get(Y2));
-   F.add(ds->ds.get(Z));
-   F.add(ds->ds.getTimeReal());
+            ArrayList<DataStateExpression> F = new ArrayList<>();
+            F.add(ds->ds.get(X));
+            F.add(ds->ds.get(Y1));
+            F.add(ds->ds.get(Y2));
+            F.add(ds->ds.get(Z));
+            F.add(ds->ds.getTimeReal());
+            F.add(ds->ds.getTimeDelta());
 
 
 
-   ArrayList<String> L = new ArrayList<>();
-   L.add("X       ");
-   L.add("Y1     ");
-   L.add("Y2      ");
-   L.add("Z      ");
-   L.add("time      ");
+            ArrayList<String> L = new ArrayList<>();
+            L.add("X       ");
+            L.add("Y1     ");
+            L.add("Y2      ");
+            L.add("Z      ");
+            L.add("time      ");
+            L.add("delta      ");
 
 
 
@@ -224,7 +228,6 @@ public class Main {
             EXPERIMENTS
 
             We propose some experiments showing how our tool can be used.
-
 
             */
 
@@ -240,13 +243,9 @@ public class Main {
             <code>size</code> of configurations, with the first sample set consisting in <code>size</code> copies of
             <code>system</code>.
             The second evolution sequence is perturbed by applying the perturbation returned by the static method
-            <code>itZ1TranslRate(x)</code> defined later. Essentially, the method returns a cyclic perturbation that
-            affects the translation rate  of gene 1:  for <code>replica</code> times, it has no effect for the first w1
-            time points, i.e., the system behaves regularly, then in the subsequent <code>w2</code> time points, the
-            translation rare is decremented by x, which impacts directly on the evolution of <code>Z1</code> and,
-            through interactions, on <code>Z2</code> and <code>Z3</code>.
-            This perturbation models protein translation deregulation.
-
+            <code>pertY (x)</code> defined later. Essentially, the method returns a perturbation that
+            cuts a portion of the molecules of Y1 at a given step. In the prey/predator interpretation by Volterra, this
+            corresponts to simulate an epidemy affecting the former population.
             For both evolution sequences, we store in .csv files some information allowing us to observe the dynamics of
             both the nominal and the perturbed system: for each time unit in [0,N-1] and for each variable, we store
             the average value that the variable assumes in the <code>size</code> configurations in the sample set
@@ -254,33 +253,60 @@ public class Main {
 
             */
 
-   System.out.println();
-   System.out.println("Simulation of nominal and perturbed system");
-   System.out.println();
+            System.out.println();
+            System.out.println("Simulation of nominal and perturbed system");
+            System.out.println();
 
 
-   int N = 300;    // length ot the evolution sequence
+            int N = 300;    // length ot the evolution sequence
 
 
 
-   double[][] plot_LX = new double[N][1];
-   double[][] plot_LY1 = new double[N][1];
-   double[][] plot_LY2 = new double[N][1];
-   double[][] plot_LZ = new double[N][1];
 
-   double[][] data = SystemState.sample(rand, F, system, N, size);
-   for (int i = 0; i<N; i++){
-    plot_LX[i][0] = data[i][0];
-    plot_LY1[i][0] = data[i][1];
-    plot_LY2[i][0] = data[i][2];
-    plot_LZ[i][0] = data[i][3];
-   }
-   Util.writeToCSV("./new_plotLX.csv",plot_LX);
-   Util.writeToCSV("./new_plotLY1.csv",plot_LY1);
-   Util.writeToCSV("./new_plotLY2.csv",plot_LY2);
-   Util.writeToCSV("./new_plotLZ.csv",plot_LZ);
+            double[][] plot_LX = new double[N][1];
+            double[][] plot_LY1 = new double[N][1];
+            double[][] plot_LY2 = new double[N][1];
+            double[][] plot_LZ = new double[N][1];
 
+            double[][] data = SystemState.sample(rand, F, system, N, size);
+            for (int i = 0; i<N; i++){
+              plot_LX[i][0] = data[i][0];
+              plot_LY1[i][0] = data[i][1];
+              plot_LY2[i][0] = data[i][2];
+              plot_LZ[i][0] = data[i][3];
+            }
+            Util.writeToCSV("./new_plotLX.csv",plot_LX);
+            Util.writeToCSV("./new_plotLY1.csv",plot_LY1);
+            Util.writeToCSV("./new_plotLY2.csv",plot_LY2);
+            Util.writeToCSV("./new_plotLZ.csv",plot_LZ);
 
+            double[][] plot_pertLX = new double[N][1];
+            double[][] plot_pertLY1 = new double[N][1];
+            double[][] plot_pertLY2 = new double[N][1];
+            double[][] plot_pertLZ = new double[N][1];
+
+            double[][] pertdata = SystemState.sample(rand, F, pertY1(N/2), system, N, size);
+            for (int i = 0; i<N; i++){
+              plot_pertLX[i][0] = pertdata[i][0];
+              plot_pertLY1[i][0] = pertdata[i][1];
+              plot_pertLY2[i][0] = pertdata[i][2];
+              plot_pertLZ[i][0] = pertdata[i][3];
+            }
+            Util.writeToCSV("./new_plotLXpert.csv",plot_pertLX);
+            Util.writeToCSV("./new_plotLY1pert.csv",plot_pertLY1);
+            Util.writeToCSV("./new_plotLY2pert.csv",plot_pertLY2);
+            Util.writeToCSV("./new_plotLZpert.csv",plot_pertLZ);
+
+            double[][] plot_LY1Y2 = new double[N][2];
+            double[][] plot_pertLY1Y2 = new double[N][2];
+            for (int i = 0; i<N; i++){
+               plot_LY1Y2[i][0] = data[i][1];
+               plot_LY1Y2[i][1] = data[i][2];
+               plot_pertLY1Y2[i][0] = pertdata[i][1];
+               plot_pertLY1Y2[i][1] = pertdata[i][2];
+            }
+            Util.writeToCSV("./new_plotLY1Y2.csv",plot_LY1Y2);
+            Util.writeToCSV("./new_plotLY1Y2pert.csv",plot_pertLY1Y2);
 
             /*
             While in the previous lines of code the average values of variables obtained step-by-step are stored in
@@ -288,14 +314,124 @@ public class Main {
             */
 
 
-   System.out.println();
-   System.out.println("Simulation of nominal system - data average values:");
-   System.out.println();
-   printAvgData(rand, L, F, system, N, size, 0, N);
-   System.out.println();
-   //System.out.println("Simulation of perturbed system - data average values:");
-   //System.out.println("");
-   //printAvgDataPerturbed(rand, L, F, system, N, size, 0, N, itZ1TranslRate(x, w1, w2, replica));
+           System.out.println();
+           System.out.println("Simulation of nominal system - data average values:");
+           System.out.println();
+           printAvgData(rand, L, F, system, N, size, 0, N);
+           System.out.println();
+           System.out.println("Simulation of perturbed system - data average values:");
+           System.out.println("");
+           printAvgDataPerturbed(rand, L, F, system, N, size, 0, N, pertY1(N/2));
+
+
+
+
+
+   /*
+
+            ESTIMATING BEHAVIORAL DISTANCES BETWEEN NOMINAL AND PERTURBED EVOLUTION SEQUENCES
+
+
+            Now we generate again a nominal and a perturbed evolution sequence, as above.
+            Then, we quantify the differences between those evolutions sequences, which corresponds to quantifying the
+            behavioural distance between the nominal and the perturbed system. The differences are quantified with
+            respect to the amount Y1 and Y2.
+
+             */
+
+
+            /*
+            In order to quantify the difference between two evolution sequences w.r.t. Yi, we need to define the
+            difference between two configurations w.r.t. Yi: given two configurations with Yi=m and Yi=n, the
+            difference between those configurations w.r.t. Yi is the value |m-n|, normalised wrt the maximal value that
+            can be assumed by Yi, so that this difference is always in [0,1].
+            Since we cannot know a priori which is the maximal value that can be assumed by Yi, we estimate it.
+
+            Therefore, we start with estimating the maximal values that can be assumed by all variables.
+            To this purpose, we generate a nominal and a perturbed evolution sequence of length 2N and collect the
+            maximal values that are assumed by the variables in all configurations in all sample sets.
+            */
+
+
+
+
+
+           System.out.println("");
+
+           System.out.println("Estimating behavioural differences between nominal and perturbed system");
+           System.out.println("");
+
+
+           System.out.println("");
+           System.out.println("Simulation of nominal system - Data maximal values:");
+           double[] dataMax = printMaxData(rand, L, F, system, N, size, 0, 2*N);
+           System.out.println("");
+           System.out.println("Simulation of perturbed system - Data maximal values:");
+           System.out.println("");
+           double[] dataMax_p = printMaxDataPerturbed(rand, L, F, system, N, size, 0, 2*N, pertY1(N/4));
+
+           double normalisationY1 = Math.max(dataMax[Y1],dataMax_p[Y1])*1.1;
+           double normalisationY2 = Math.max(dataMax[Y2],dataMax_p[Y2])*1.1;
+
+
+
+
+            /*
+            The following instruction allows us to create the evolution sequence <code>sequence_p</code>, which is
+            obtained from the evolution sequence <code>sequence</code> by applying a perturbation, where:
+            - as above, the perturbation is returned by the static method <code>pertY1()</code> defined later
+            - the perturbation is applied at step N/2
+            - the sample sets of configurations in <code>sequence_p</code> have a cardinality which corresponds to that
+            of <code>sequence</code> multiplied by <code>scale>/code>
+            */
+
+
+            int scale=2;
+            EvolutionSequence sequence_p = sequence.apply(pertY1(N/2),0,scale);
+
+
+            /*
+            The following lines of code first defines two atomic distances between evolution sequences, named
+            <code>atomicYi</code> for i=1,2. Then, these distances are evaluated, time-point by time-point, over
+            evolution sequence <code>sequence</code> and its perturbed version <code>sequence_p</code> defined above.
+            Finally, the time-point to time-point values of the distances are stored in .csv files.
+            Technically, <code>distanceYi</code> is an atomic distance in the sense that it is an instance of
+            class <code>AtomicDistanceExpression</code>, which consists in a data state expression,
+            which maps a data state to a number, or rank, and a binary operator. As already discussed, in this case,
+            given two configurations, the data state expression allow us to get the normalised value of Yi,
+            which is a value in [0,1], from both configuration, and the binary operator gives us their difference, which,
+            intuitively, is the difference with respect to the level of Zi between the two configurations.
+            This distance will be lifted to two sample sets of configurations, those obtained from <code>sequence</code> and
+            <code>sequence_p</code> at the same step.
+            */
+
+            int leftBound = 0;
+            int rightBound = N;
+
+
+            AtomicDistanceExpression atomicY1 = new AtomicDistanceExpression(ds->ds.get(Y1)/normalisationY1,(v1, v2) -> Math.abs(v2-v1));
+
+            AtomicDistanceExpression atomicY2 = new AtomicDistanceExpression(ds->ds.get(Y2)/normalisationY2,(v1, v2) -> Math.abs(v2-v1));
+
+
+            double[][] direct_evaluation_atomic_Y1 = new double[rightBound-leftBound][1];
+            double[][] direct_evaluation_atomic_Y2 = new double[rightBound-leftBound][1];
+
+            for (int i = 0; i<(rightBound-leftBound); i++){
+              direct_evaluation_atomic_Y1[i][0] = atomicY1.compute(i+leftBound, sequence, sequence_p);
+              direct_evaluation_atomic_Y2[i][0] = atomicY2.compute(i+leftBound, sequence, sequence_p);
+            }
+
+            Util.writeToCSV("./new_Latomic_Y1.csv",direct_evaluation_atomic_Y1);
+            Util.writeToCSV("./new_Latomic_Y2.csv",direct_evaluation_atomic_Y2);
+
+
+
+
+
+
+
+
 
 
 
@@ -588,6 +724,35 @@ public class Main {
   return updates;
 
  }
+
+
+
+
+
+
+
+
+
+
+    /*
+    The following method returns a perturbation. In particular this is an atomic perturbation, that
+    prunes at most half of the molecules of Y1.
+
+     */
+
+
+ public static Perturbation pertY1(int step){
+  return new AtomicPerturbation(step,(rg,ds)->ds.apply(cutY1(rg,ds)));
+ }
+ private static List<DataStateUpdate> cutY1(RandomGenerator rg, DataState state){
+  List<DataStateUpdate> updates = new LinkedList<>();
+  updates.add(new DataStateUpdate(Y1,state.get(Y1)*0.5));
+     // updates.add(new DataStateUpdate(Y2,state.get(Y2)*0.5));
+  return updates;
+ }
+
+
+
 
 
 
