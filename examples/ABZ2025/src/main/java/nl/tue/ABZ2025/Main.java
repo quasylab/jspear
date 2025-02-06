6ee13c5d1e8d21198986155dc753296b0a9e8877
ValentinaCastiglioni <vale.castiglioni@gmail.com>
2025-02-06 10:48:21 +0100

    Added IOException to main fail

--- examples/ABZ2025/src/main/java/nl/tue/ABZ2025/Main.java
+++ examples/ABZ2025/src/main/java/nl/tue/ABZ2025/Main.java
@@ -1,9 +1,16 @@
 package nl.tue.ABZ2025;
 
 import Scenarios.SingleLaneTwoCars;
+import Scenarios.TwoLanesTwoCars;
+
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
-        new SingleLaneTwoCars();
+    public static void main(String[] args) throws IOException {
+        try{
+            new TwoLanesTwoCars();
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+        }
     }
 }
