cd8cc306a619a086dceb06b5976f3a061fb4233f
Sebastian Betancourt <j.s.betancourt.arias@tue.nl>
2026-03-09 11:04:34 +0100

    Monitors: test

--- lib/src/test/java/nl/tue/Monitoring/UnboundedDefaultUnboundedUntilMonitorTest.java
+++ lib/src/test/java/nl/tue/Monitoring/UnboundedDefaultUnboundedUntilMonitorTest.java
@@ -26,10 +26,7 @@ package nl.tue.Monitoring;
 import it.unicam.quasylab.jspear.*;
 import it.unicam.quasylab.jspear.controller.Controller;
 import it.unicam.quasylab.jspear.controller.ControllerRegistry;
-import it.unicam.quasylab.jspear.distl.DisTLFormula;
-import it.unicam.quasylab.jspear.distl.DoubleSemanticsVisitor;
-import it.unicam.quasylab.jspear.distl.TargetDisTLFormula;
-import it.unicam.quasylab.jspear.distl.UntilDisTLFormula;
+import it.unicam.quasylab.jspear.distl.*;
 import it.unicam.quasylab.jspear.ds.DataState;
 import it.unicam.quasylab.jspear.ds.DataStateFunction;
 import it.unicam.quasylab.jspear.ds.DataStateUpdate;
@@ -39,6 +36,7 @@ import nl.tue.Monitoring.Default.DefaultMonitorBuilder;
 import nl.tue.Monitoring.Default.DefaultUDisTLMonitor;
 import org.apache.commons.math3.random.RandomGenerator;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
@@ -60,6 +58,7 @@ class UnboundedDefaultUnboundedUntilMonitorTest {
 
     static final int t = 0;
     static final int x = 1;
+    static final int y = 2;
 
     @BeforeAll
     static void setup(){
@@ -155,7 +154,150 @@ class UnboundedDefaultUnboundedUntilMonitorTest {
 
     }
 
+    @Test
+    void monotononicityCounterexample() {
+        Function<Double, Double> sine = t -> (Math.sin(t/3.0)+1)/2.0;  // function that goes /\/\/\/\/ with period around 10
+        Function<Double, Double> sineShifted = t -> (Math.sin((t/3.0)+Math.PI)+1)/2.0;  // function that goes /\/\/\/\/ with period around 10
+        DataStateFunction environment = (rg, ds) -> ds.apply(List.of(
+                new DataStateUpdate(t, ds.get(t) + 1),
+//                new DataStateUpdate(x, ds.get(t) % 2 == 0 ? 1.5- (1/Math.sqrt(Math.log(ds.get(t)+Math.exp(1)))): (1/Math.sqrt(Math.log(ds.get(t)+Math.exp(1)))) - 0.5)));
+                new DataStateUpdate(x, sine.apply(ds.get(t))),
+                new DataStateUpdate(y, sineShifted.apply(ds.get(t)))));
+        Function<RandomGenerator, SystemState> system = rg ->
+                new ControlledSystem(idleController, environment, new DataState(new double[]{5, sine.apply(5.0), sineShifted.apply(5.0)}));
+        DefaultRandomGenerator rng = new DefaultRandomGenerator();
+        rng.setSeed(seed);
+        EvolutionSequence sequence = new EvolutionSequence(rng, system, ES_SAMPLE_SIZE);
+
+        int TEST_LIMIT = 10;
+        // mu is a dirac dist around (0,0) and penalty fn P((t,x)) = ds(x)
+        DataStateFunction mu = (rg, ds) -> ds.apply(
+                List.of(new DataStateUpdate(t, 0),
+                        new DataStateUpdate(x, 0.0),
+                        new DataStateUpdate(y, 0.0)
+                ));
+        DisTLFormula atomicUp = new TargetDisTLFormula(mu, ds -> ds.get(y), 0.0);
+        DisTLFormula atomicDown = new TargetDisTLFormula(mu, ds -> ds.get(x), 0.0);
+        DisTLFormula goesUp = new AlwaysDisTLFormula(atomicUp,0,8);
+        DisTLFormula goesDown = new EventuallyDisTLFormula(atomicDown,0,8);
+        DisTLFormula goesUpAndDown = new ConjunctionDisTLFormula(goesDown, goesUp);
+
+        int semanticsEvalTimestep = 0;
+        DefaultMonitorBuilder defaultMonitorBuilder = new DefaultMonitorBuilder(SAMPLE_SIZE, false);
+
+        DefaultUDisTLMonitor mAlways = defaultMonitorBuilder.build(goesUp, semanticsEvalTimestep);
+        DefaultUDisTLMonitor mEvent = defaultMonitorBuilder.build(goesDown, semanticsEvalTimestep);
+        DefaultUDisTLMonitor mCon = defaultMonitorBuilder.build(goesUpAndDown, semanticsEvalTimestep);
+        mAlways.setRandomGeneratorSeed(seed);
+        mEvent.setRandomGeneratorSeed(seed);
+        mCon.setRandomGeneratorSeed(seed);
+        int from = 0;
+        for (int i = 0; i < TEST_LIMIT; i++) {
+            SampleSet<PerceivedSystemState> observationSampleSet = sequence.getAsPerceivedSystemStates(i);
+            DataState sample = observationSampleSet.stream().toList().get(1).getDataState();
+
+            OptionalDouble mEvalAlways = mAlways.evalNext(observationSampleSet);
+            OptionalDouble mEvalEvent = mEvent.evalNext(observationSampleSet);
+            OptionalDouble mEvalCon = mCon.evalNext(observationSampleSet);
+
+            System.out.printf("t=%d, x=%.4f, y=%.4f, mon output: always=%.4f, event=%.4f con=%.4f %n", i, sample.get(x), sample.get(y), mEvalAlways.isPresent() ? mEvalAlways.getAsDouble() : 9.0, mEvalEvent.isPresent() ? mEvalEvent.getAsDouble() : 9.0, mEvalCon.isPresent() ? mEvalCon.getAsDouble() : 9.0);
 
 
+        }
+    }
+
+    @Test
+    void tarsCounterexample() {
+        DataStateFunction environment = (rg, ds) -> ds.apply(List.of(
+                new DataStateUpdate(t, ds.get(t) + 1),
+                new DataStateUpdate(x, ds.get(t) % 2 == 0 ? 1 : 0)));
+        Function<RandomGenerator, SystemState> system = rg ->
+                new ControlledSystem(idleController, environment, new DataState(new double[]{0.0, 0.0}));
+        DefaultRandomGenerator rng = new DefaultRandomGenerator();
+        rng.setSeed(seed);
+        EvolutionSequence sequence = new EvolutionSequence(rng, system, ES_SAMPLE_SIZE);
+
+        int TEST_LIMIT = 1000;
+        // mu is a dirac dist around (0,0) and penalty fn P((t,x)) = ds(x)
+        DataStateFunction mu = (rg, ds) -> ds.apply(
+                List.of(new DataStateUpdate(t, 0),
+                        new DataStateUpdate(x, 0.0)
+                ));
+        DisTLFormula atomic = new TargetDisTLFormula(mu, ds -> ds.get(x), 0.0);
+        DisTLFormula T = new TrueDisTLFormula();
+        UDisTLFormula counterex = new UnboundedUntiluDisTLFormula(T, new NegationDisTLFormula(new UnboundedUntiluDisTLFormula(T, atomic)));
+
+
+        int semanticsEvalTimestep = 0;
+        DefaultMonitorBuilder defaultMonitorBuilder = new DefaultMonitorBuilder(SAMPLE_SIZE, false);
+
+        DefaultUDisTLMonitor m = defaultMonitorBuilder.build(counterex, semanticsEvalTimestep);
+        m.setRandomGeneratorSeed(seed);
+        int from = 0;
+        double previous = -9.0;
+        for (int i = 0; i < TEST_LIMIT; i++) {
+            SampleSet<PerceivedSystemState> observationSampleSet = sequence.getAsPerceivedSystemStates(i);
+            DataState sample = observationSampleSet.stream().toList().get(1).getDataState();
+
+            OptionalDouble mEval = m.evalNext(observationSampleSet);
+            if (mEval.isPresent()){
+                double current = mEval.getAsDouble();
+                if(current == previous){
+                    System.out.println("Monotonicity encountered at t="+i);
+                }
+                previous = current;
+            }
+//            System.out.printf("t=%d, x=%.4f, mon output:%.4f %n", i, sample.get(x), mEval.isPresent() ? mEval.getAsDouble() : 9.0);
+
+
+        }
+    }
+
+    @Test
+    void tarsCounterexampleWBounded() {
+        DataStateFunction environment = (rg, ds) -> ds.apply(List.of(
+                new DataStateUpdate(t, ds.get(t) + 1),
+                new DataStateUpdate(x, ds.get(t) % 2 == 0 ? 1 : 0)));
+        Function<RandomGenerator, SystemState> system = rg ->
+                new ControlledSystem(idleController, environment, new DataState(new double[]{0.0, 0.0}));
+        DefaultRandomGenerator rng = new DefaultRandomGenerator();
+        rng.setSeed(seed);
+        EvolutionSequence sequence = new EvolutionSequence(rng, system, ES_SAMPLE_SIZE);
+
+        int TEST_LIMIT = 1000;
+        // mu is a dirac dist around (0,0) and penalty fn P((t,x)) = ds(x)
+        DataStateFunction mu = (rg, ds) -> ds.apply(
+                List.of(new DataStateUpdate(t, 0),
+                        new DataStateUpdate(x, 0.0)
+                ));
+        DisTLFormula atomic = new TargetDisTLFormula(mu, ds -> ds.get(x), 0.0);
+        DisTLFormula T = new TrueDisTLFormula();
+        UDisTLFormula counterex = new UnboundedUntiluDisTLFormula(T, new NegationDisTLFormula(new UntilDisTLFormula(T, 0,2, atomic)));
+
+
+        int semanticsEvalTimestep = 0;
+        DefaultMonitorBuilder defaultMonitorBuilder = new DefaultMonitorBuilder(SAMPLE_SIZE, false);
+
+        DefaultUDisTLMonitor m = defaultMonitorBuilder.build(counterex, semanticsEvalTimestep);
+        m.setRandomGeneratorSeed(seed);
+        int from = 0;
+        double previous = -9.0;
+        for (int i = 0; i < TEST_LIMIT; i++) {
+            SampleSet<PerceivedSystemState> observationSampleSet = sequence.getAsPerceivedSystemStates(i);
+            DataState sample = observationSampleSet.stream().toList().get(1).getDataState();
+
+            OptionalDouble mEval = m.evalNext(observationSampleSet);
+            if (mEval.isPresent()){
+                double current = mEval.getAsDouble();
+                if(current == previous){
+                    System.out.println("Monotonicity encountered at t="+i);
+                }
+                previous = current;
+            }
+            System.out.printf("t=%d, x=%.4f, mon output:%.4f %n", i, sample.get(x), mEval.isPresent() ? mEval.getAsDouble() : 9.0);
+
+
+        }
+    }
 }
 
